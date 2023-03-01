94d239162dc83b551ebeb793002a7d3415e09d30
Phil Brockman <phil.brockman@gmail.com>
2023-03-01 13:10:48 -0600

    Update index.ts

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,42 +1,21 @@
 // Listen for messages from the popup script
+// background.js
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  // if (request.type === 'injectPopup') {
-  //   // Get the URL of the popup page
-  //   var popupUrl = chrome.runtime.getURL('popup.html')
-  //   // Fetch the contents of the popup page as a string
-  //   fetch(popupUrl)
-  //     .then((response) => response.text())
-  //     .then((popupContents) => {
-  //       console.log('popupContents', popupContents)
-  //       // Inject the popup contents into the active tab
-  //       // chrome.tabs.executeScript(
-  //       //   request.tabId,
-  //       //   {
-  //       //     code: `
-  //       //   // Create a container element
-  //       //   var container = document.createElement('div');
-  //       //   container.style.cssText = 'position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); z-index: 9999;';
-  //       //   // Create a popup element
-  //       //   var popup = document.createElement('div');
-  //       //   popup.style.cssText = 'position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: white; padding: 10px;';
-  //       //   // Add the contents of the popup to the popup element
-  //       //   popup.innerHTML = '${popupContents}';
-  //       //   // Add the popup element to the container element
-  //       //   container.appendChild(popup);
-  //       //   // Add the container element to the document body
-  //       //   document.body.appendChild(container);
-  //       // `,
-  //       //   },
-  //       //   function (result) {
-  //       //     sendResponse(result)
-  //       //   },
-  //       // )
-  //     })
-  //     .catch((error) => {
-  //       console.error(error)
-  //       sendResponse({ error: error.message })
-  //     })
-  //   return true
+  // if (request.action === 'injectPopup') {
+  //   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  //     chrome.tabs.executeScript(
+  //       tabs[0].id,
+  //       { code: 'document.body.innerHTML += "<div id=\\"extension-popup\\"></div>";' },
+  //       function () {
+  //         chrome.runtime.sendMessage({ action: 'getPopupContent' }, function (response) {
+  //           chrome.tabs.executeScript(tabs[0].id, {
+  //             code: `document.querySelector("#extension-popup").innerHTML = \`${response}\`;`,
+  //           })
+  //         })
+  //       },
+  //     )
+  //   })
   // }
 })
 
