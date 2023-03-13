24a5dab15394beeec1de3047dbb2491647133e62
Phil Brockman <phil.brockman@gmail.com>
2023-03-13 16:59:30 -0500

    Update Popup.tsx

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -12,27 +12,6 @@ import Settings from './Modal/Settings'
 const PREFERRED_HIDDEN_PIECES = 'PREFERRED_HIDDEN_PIECES_BY_COLOR'
 const IS_ACTIVE = 'IS_ACTIVE'
 
-// function areElementsOverlapping(element1: HTMLElement, element2: HTMLElement) {
-//   const rect1 = element1.getBoundingClientRect()
-//   const rect2 = element2.getBoundingClientRect()
-
-//   const overlapX = Math.max(
-//     0,
-//     Math.min(rect1.right, rect2.right) - Math.max(rect1.left, rect2.left),
-//   )
-//   const overlapY = Math.max(
-//     0,
-//     Math.min(rect1.bottom, rect2.bottom) - Math.max(rect1.top, rect2.top),
-//   )
-
-//   const area1 = (rect1.right - rect1.left) * (rect1.bottom - rect1.top)
-//   const area2 = (rect2.right - rect2.left) * (rect2.bottom - rect2.top)
-
-//   const overlapArea = overlapX * overlapY
-//   const totalArea = area1 + area2 - overlapArea
-
-//   return overlapArea / totalArea >= 0.95
-// }
 const basicObserver = ({ callback }: { callback: (target: any) => void }) => {
   const observer = new MutationObserver((mutationsList, observer) => {
     for (const mutation of mutationsList) {
